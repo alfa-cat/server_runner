6b90335951952ce980c0cfede7f68884f9aca37b
alfa-cat <alfa-cat@users.noreply.github.com>
2026-07-25 22:53:05 +0700

    feat(data): исправлен FileManager. Добавлен атомарный writeFile с очисткой и рекурсивный обход папок listFiles для сессий

--- src/data/file_manager.cpp
+++ src/data/file_manager.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <ostream>
 #include <sstream>
+#include <string>
+#include <vector>
+#include <filesystem>
 
 std::string FileManager::readFile(const std::string& filePath) {
     std::cerr << "Trying to open: " << filePath << std::endl;
@@ -14,4 +17,34 @@ std::string FileManager::readFile(const std::string& filePath) {
     std::stringstream buffer;
     buffer << file.rdbuf();
     return buffer.str();
+}
+
+
+bool FileManager::writeFile(const std::string& filePath, const std::string& content) {
+        std::ofstream file(filePath, std::ios::out | std::ios::trunc);
+        if (!file.is_open()) {
+            return false;
+        }
+
+        file << content;
+        
+        if (!file) {
+            return false;
+        }
+
+        return true;
+}
+
+std::vector<std::string> FileManager::listFiles(const std::string& dirPath) {
+    std::vector<std::string> allFiles;
+
+    if (!std::filesystem::exists(dirPath) || !std::filesystem::is_directory(dirPath)) {
+        return allFiles;
+    }
+    for (const auto& entry : std::filesystem::recursive_directory_iterator(dirPath)) {
+        if (std::filesystem::is_regular_file(entry.path())) {
+            allFiles.push_back(entry.path().string());
+        }
+    }
+    return allFiles;
 }
--- src/data/file_manager.h
+++ src/data/file_manager.h
@@ -6,5 +6,5 @@ class FileManager { // создаем класс
 public:
     std::string readFile(const std::string& filePath); // тута мы читаем файл, очевидно к нему должен быть путь, отсюда и параметр filePath
     bool writeFile(const std::string& filePath, const std::string& content); // тута у нас запись текста, путь куда записать, и контент
-    std::vector<std::string> listFiles(const std::string& dir); // тута мы говорим что будет видеть файлы из директории
+    std::vector<std::string> listFiles(const std::string& dirPath); // тута мы говорим что будет видеть файлы из директории
 };
